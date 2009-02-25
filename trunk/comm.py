68321115d4a165fd3b54893ea2d9db6bee86fddb
king.seth <king.seth@d0075b96-fc5a-11dd-a320-db7d8e7a638f>
2009-02-25 17:57:37 +0000

    Change comm file to work with python 2.5 from 3.0 pyserial works now.

--- trunk/comm.py
+++ trunk/comm.py
@@ -1,9 +1,12 @@
 import time
 import serial
 
-ser = serial.Serial(1, 38400)
+ser = serial.Serial(2, 9600)
 seconds = int(time.time())
 timestr = str(seconds) + '\n'
-print(timestr)
-print(time.ctime(seconds))
-ser.write(str(int(time.time())))
+print timestr 
+print time.ctime(seconds) 
+
+print(ser.readline())
+#print ser.write("Test\n")
+
