d0868958e47ac944f5bb06deaa9556459e3ef9e1
king.seth <king.seth@d0075b96-fc5a-11dd-a320-db7d8e7a638f>
2009-02-26 04:01:29 +0000

    continued comm testing

--- trunk/comm.py
+++ trunk/comm.py
@@ -1,13 +1,20 @@
 import time
 import serial
 
-ser = serial.Serial(2, 9600)
+ser = serial.Serial(7, 9600,timeout=5)
 seconds = int(time.time())
 timestr = str(seconds) + '\n'
 print timestr 
-print time.ctime(seconds) 
+#print time.ctime(seconds) 
 
 
 ser.write(timestr)
 print(ser.readline())
+print(ser.readline())
+print(ser.readline())
+print(ser.readline())
+print(ser.readline())
+print(ser.readline())
+print(ser.readline())
+
 
