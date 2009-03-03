6d369acec85f2ce0309d66fcc2bdc52033bed0be
king.seth <king.seth@d0075b96-fc5a-11dd-a320-db7d8e7a638f>
2009-03-03 18:04:22 +0000

    Test comm program working

--- trunk/comm.py
+++ trunk/comm.py
@@ -1,20 +1,17 @@
 import time
+from time import sleep
 import serial
 
-ser = serial.Serial(7, 9600,timeout=5)
+ser = serial.Serial(3, 9600)
 seconds = int(time.time())
 timestr = str(seconds) + '\n'
+print "Unix Time:"
 print timestr 
 #print time.ctime(seconds) 
 
 
 ser.write(timestr)
+sleep(1) #sleeps for 1 seconds
+print "Arduino Time:"
 print(ser.readline())
-print(ser.readline())
-print(ser.readline())
-print(ser.readline())
-print(ser.readline())
-print(ser.readline())
-print(ser.readline())
-
 
